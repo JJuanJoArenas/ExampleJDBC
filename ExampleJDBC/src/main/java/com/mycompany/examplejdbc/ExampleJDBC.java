4f0d5d9f4f51f51eae34e7f8be9b991a34834019
JuanJo Arenas <dragonex579@gmail.com>
2025-03-13 08:10:25 -0500

    Configuración proyecto
    Se añade dependencia de MySQL connector J al pom y se agregan tags de documentación

---
+++ ExampleJDBC/src/main/java/com/mycompany/examplejdbc/ExampleJDBC.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examplejdbc;
+
+/**
+ * Fecha: 13/03/2025
+ * @author JuanJo
+ */
+public class ExampleJDBC {
+    
+}
